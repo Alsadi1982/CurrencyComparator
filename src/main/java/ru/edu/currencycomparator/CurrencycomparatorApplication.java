26b48269f5938f74a20bbb0b0ff1039de1b547a7
Alex Sadikov <sav1982@inbox.ru>
2022-05-31 22:15:12 +0300

    start with Spring initializr

---
+++ src/main/java/ru/edu/currencycomparator/CurrencycomparatorApplication.java
@@ -0,0 +1,13 @@
+package ru.edu.currencycomparator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencycomparatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencycomparatorApplication.class, args);
+	}
+
+}
