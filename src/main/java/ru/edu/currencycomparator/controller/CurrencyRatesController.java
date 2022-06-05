b0aeb84209df076e5e752d2f1f6ac314105eb56a
Alex Sadikov <sav1982@inbox.ru>
2022-06-06 00:06:54 +0300

    add services and controller

--- src/main/java/ru/edu/currencycomparator/controller/CurrencyRatesController.java
+++ src/main/java/ru/edu/currencycomparator/controller/CurrencyRatesController.java
@@ -2,26 +2,68 @@ package ru.edu.currencycomparator.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import ru.edu.currencycomparator.model.CurrencyRates;
 import ru.edu.currencycomparator.service.CurrencyRatesServiceImp;
+import ru.edu.currencycomparator.service.GiphyServiceImp;
 
-@RestController
+/**
+ * Контроллер
+ */
+@Controller
 @RequestMapping("/")
 public class CurrencyRatesController {
 
-    @Value("${openexchangerates.org.app_id}")
-    private String appID;
+    @Value("${openexchangerates.org.base_currency}")
+    private String buseCurrency;
+
+    @Value("${giphy.com.rich_tag}")
+    private String richTag;
+
+    @Value("${giphy.com.broken_tag}")
+    private String brokenTag;
+
+    @Value("${giphy.com.equal_tag}")
+    private String equalTag;
+
+    @Value("${giphy.com.error_tag}")
+    private String errorTag;
+
+    private final CurrencyRatesServiceImp currencyRatesServiceImp;
+    private final GiphyServiceImp giphyServiceImp;
 
     @Autowired
-    private CurrencyRatesServiceImp currencyRatesServiceImp;
+    public CurrencyRatesController(CurrencyRatesServiceImp currencyRatesServiceImp, GiphyServiceImp giphyServiceImp) {
+        this.currencyRatesServiceImp = currencyRatesServiceImp;
+        this.giphyServiceImp = giphyServiceImp;
+    }
 
 
+    /**
+     *  Отрисовывает страничку с гифкой в браузере
+     * @param model
+     * @return
+     */
     @GetMapping
-    public CurrencyRates index (){
-        return currencyRatesServiceImp.getTodayRate(appID);
+    public String index (Model model){
+        String gifUrl = giphyServiceImp.getGif(errorTag);
+
+        Double prevRate = currencyRatesServiceImp.getPreviousRate(buseCurrency);
+        Double presentRate = currencyRatesServiceImp.getPresentRate(buseCurrency);
+
+        if (prevRate > presentRate){
+            gifUrl = giphyServiceImp.getGif(brokenTag);
+        } else if ( prevRate < presentRate) {
+            gifUrl = giphyServiceImp.getGif(richTag);
+        }else if( prevRate.equals(presentRate)){
+            gifUrl = giphyServiceImp.getGif(equalTag);
+        }
+
+        model.addAttribute("rate", prevRate);
+        model.addAttribute("gifUrl", gifUrl);
+        return "welcome";
     }
 
 
--- src/main/java/ru/edu/currencycomparator/service/CurrencyRatesServiceImp.java
+++ src/main/java/ru/edu/currencycomparator/service/CurrencyRatesServiceImp.java
@@ -1,23 +1,66 @@
 package ru.edu.currencycomparator.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ru.edu.currencycomparator.client.CurrencyFeignClient;
 import ru.edu.currencycomparator.model.CurrencyRates;
 
+import java.time.LocalDate;
+
+/**
+ * Сервис для работы с запросами openexchangerates.org
+ */
 @Service
 public class CurrencyRatesServiceImp implements CurrencyRatesService{
 
+    @Value("${openexchangerates.org.app_id}")
+    private String appID;
+
+    private final CurrencyFeignClient currencyFeignClient;
+
     @Autowired
-    private CurrencyFeignClient currencyFeignClient;
-
-    @Override
-    public CurrencyRates getYesterdayRate(String data, String appID) {
-        return currencyFeignClient.getYesterdayRate(data, appID);
+    public CurrencyRatesServiceImp(CurrencyFeignClient currencyFeignClient) {
+        this.currencyFeignClient = currencyFeignClient;
     }
 
+
+    /**
+     * Возвращает значение курса валюты за предыдущий день
+     * @param baseCurrency наименование волюты курс которой нас интересует, задается в файле application.properties
+     * @return значение курса валюты
+     */
     @Override
-    public CurrencyRates getTodayRate(String appID) {
-        return currencyFeignClient.getTodayRate(appID);
+    public Double getPreviousRate(String baseCurrency) {
+        String yesterdayDate = this.getYesterdayDate();
+        CurrencyRates previousRate = currencyFeignClient.getYesterdayRate(yesterdayDate, appID);
+        return previousRate.getRates().get(baseCurrency);
     }
+
+
+    /**
+     * Возвращает актуалный курс валюты
+     * @param baseCurrency наименование волюты курс которой нас интересует, задается в файле application.properties
+     * @return значение курса валюты
+     */
+    @Override
+    public Double getPresentRate(String baseCurrency) {
+        CurrencyRates presentRate = currencyFeignClient.getTodayRate(appID);
+
+        return presentRate.getRates().get(baseCurrency);
+    }
+
+    /**
+     * Вспомогательная функция для рассчета даты предыдущего дня
+     * @return дата предыдущего дня в формате YYYY-MM-DD
+     */
+    private String getYesterdayDate() {
+
+        LocalDate now = LocalDate.now();
+        LocalDate yesterday = now.minusDays(1);
+        System.out.println(yesterday);
+
+        return yesterday.toString();
+    }
+
 }
